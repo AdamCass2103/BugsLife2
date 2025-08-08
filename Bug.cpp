3c99b479cfe46765dc486fe196b51430e872cad3
Adam Cassidy <D00265095@student.dkit.ie>
2025-08-08 15:01:49 +0100

    bug

---
+++ Bug.cpp
@@ -0,0 +1,5 @@
+//
+// Created by student on 08/08/2025.
+//
+
+#include "Bug.h"
---
+++ Bug.h
@@ -0,0 +1,38 @@
+#ifndef BUG_H
+#define BUG_H
+
+#include <list>
+#include "Position.h"
+#include "Direction.h"
+
+class Bug {
+protected:
+    int id;
+    Position position;
+    Direction direction;
+    int size;
+    bool alive;
+    std::list<Position> path;
+
+    bool isWayBlocked() const;
+    static Direction randomDirection();
+    static Position nextPosition(const Position& pos, Direction dir, int step);
+    static bool inBounds(const Position& pos);
+
+public:
+    Bug(int id, Position pos, Direction dir, int size);
+    virtual ~Bug();
+
+    int getId() const;
+    Position getPosition() const;
+    int getSize() const;
+    bool isAlive() const;
+    void setAlive(bool status);
+    void growBy(int amount);
+    const std::list<Position>& getPath() const;
+
+    virtual void move() = 0;
+    virtual std::string getType() const = 0;
+};
+
+#endif
