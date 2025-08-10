62631d7bc3cacd7ac2b3df846aa7509b880f93c4
Adam Cassidy <D00265095@student.dkit.ie>
2025-08-10 17:18:24 +0100

    fixs

---
+++ Direction.h
@@ -0,0 +1,11 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+enum class Direction {
+    NORTH = 0,
+    EAST  = 1,
+    SOUTH = 2,
+    WEST  = 3
+};
+
+#endif // DIRECTION_H
--- POSITION.h
+++ POSITION.h
@@ -4,8 +4,7 @@
 struct Position {
     int x;
     int y;
-
     Position(int xPos = 0, int yPos = 0) : x(xPos), y(yPos) {}
 };
 
-#endif
+#endif // POSITION_H
